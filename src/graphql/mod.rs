54498e1f804846950f877f376cc221655396d6c8
linda paolieri <linda.paolieri@webcom-tlc.com>
2023-12-22 12:17:53 +0100

    wip

--- src/graphql/mod.rs
+++ src/graphql/mod.rs
@@ -1,12 +1,18 @@
-use async_graphql::Object;
+use async_graphql::*;
 pub mod weather;
 pub use weather::*;
 
 pub struct Query;
 
+#[derive(InputObject)]
+pub struct Coordinate {
+    latitude: f64,
+    longitude: f64
+}
+
 #[Object]
 impl Query {
-    async fn howdy(&self) -> &'static str {
-        howdy()
+    async fn location_weather(&self, coordinate: Coordinate) -> Result<String, reqwest::Error>  {
+        location_weather(coordinate).await
     }
 }
--- src/graphql/weather/service.rs
+++ src/graphql/weather/service.rs
@@ -1,3 +1,34 @@
-pub fn howdy() -> &'static str {
-        "partner"
+use crate::graphql::Coordinate;
+use serde::Deserialize;
+#[derive(Deserialize)]
+#[derive(Debug)]
+struct WeatherApiResponse {
+   current:CurrentWeather
+}
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+struct CurrentWeather {
+   weather:Vec<Weather>
+}
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+struct Weather  {
+   main:String
+}
+
+pub async fn location_weather(coordinate:Coordinate) -> Result<String, reqwest::Error> {
+        let request_url = format!("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={key}",
+                              lat = coordinate.latitude,
+                              lon = coordinate.longitude,
+                              key = "");
+    println!("{}", request_url);
+   
+    let response = reqwest::get(&request_url).await?;
+    
+    let weather: WeatherApiResponse = response.json().await?;
+    println!("{:?}", weather);
+
+ Ok(weather.current.weather[0].main)
     }
